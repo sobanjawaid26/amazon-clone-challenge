e26259a5c24fb273b819d81b2a5d3bd45607c111
sobanjawaid26 <sobanjawaid26@gmail.com>
2020-11-06 16:53:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Checkout from './components/checkout/Checkout'
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+        <Route path="/checkout">
+              <Header />
+              <Checkout />
+          </Route>
+          <Route path="/">
+              <Header />
+              <Home />
+          </Route>
+          
+        </Switch>
+        
+    </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCh6cF8DPprQvS2e3otYSZl9764icujBN0",
+  authDomain: "challenge-8e1bb.firebaseapp.com",
+  databaseURL: "https://challenge-8e1bb.firebaseio.com",
+  projectId: "challenge-8e1bb",
+  storageBucket: "challenge-8e1bb.appspot.com",
+  messagingSenderId: "215112435963",
+  appId: "1:215112435963:web:5e79027c9a177762dbc85e"
+};
